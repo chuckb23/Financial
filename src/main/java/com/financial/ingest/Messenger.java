616be24e10f34e31ae173c6cf1570e5f5d720e20
Charles Beaudoin <charles.beaudoin@email.wsu.edu>
2017-10-09 11:10:06 -0700

    Add base ingest class

--- src/main/java/com/financial/ingest/Messenger.java
+++ src/main/java/com/financial/ingest/Messenger.java
@@ -4,7 +4,10 @@ public class Messenger {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		StockIngest stockMessage = new StockIngest("ESV", "20150313000000");
+		StockIngest stockMessage = new StockIngest("20150313000000");
+		BitcoinIngest bitcoinMessage = new BitcoinIngest();
+		//bitcoinMessage.ingestPrice("20150313000000");
+		//stockMessage.ingestAll();
 		//stockMessage.getQuote();
 
 	}
